a7d96728aae5e535bc65ed5b505403a3d68a42c1
abhishekthakurniit <abhirajput0303@gmail.com>
2016-12-26 06:36:06 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,14 @@
+var a;
+var b;
+var c=46;
+var d= 66;
+function add()
+{
+	var a = 45;
+	var b = 34;
+console.log (a);
+console.log (b);
+console.log (c);
+console.log (d);
+}add()
+
---
+++ index.js
@@ -0,0 +1,14 @@
+var a=5
+console.log(a)
+function add()
+{
+	console.log(a);
+	console.log(c);
+	var a=3;
+	var b=6;
+	console.log(a)
+	console.log(b)
+}
+add();
+console.log(a)
+console.log(b)
